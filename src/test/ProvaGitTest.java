51381fb1b7963a9e030d0c0d04af3253edf8d481
Frenk3D <61878014+Frenk3D@users.noreply.github.com>
2022-03-20 15:48:11 +0100

    Create ProvaGitTest.java

---
+++ src/test/ProvaGitTest.java
@@ -0,0 +1,21 @@
+
+package it.polimi.ingsw;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProvaGitTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
