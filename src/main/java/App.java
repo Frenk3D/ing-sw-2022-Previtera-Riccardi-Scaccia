128597409acdd5178636321a677422b2004315ab
Nicolò Scaccia <nicolo.scaccia@mail.polimi.it>
2022-04-08 15:40:41 +0200

    Implementation of some Test cases and other configs. Fra and Nico

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+/** Deselect always .idea and another .iml files with pull/push to avoid problems!!! (ok with .gitignore)
+ * Hello world!
+ * Game: Eryantis
+ * ok Fra, ok Nic, ok Marco with git
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw;
+//we do not use assume statements because we don't need them, assertTrue gives problems so we use asssertEquals with true
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,7 +8,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class ProvaGitTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
