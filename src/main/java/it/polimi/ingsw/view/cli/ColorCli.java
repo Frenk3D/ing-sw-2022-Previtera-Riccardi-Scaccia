cee43ca1a8b0d192724610efa3aa4e508ebb2e05
Francesco <61878014+Frenk3D@users.noreply.github.com>
2022-05-19 21:33:54 +0200

    Update ColorCli.java

--- src/main/java/it/polimi/ingsw/view/cli/ColorCli.java
+++ src/main/java/it/polimi/ingsw/view/cli/ColorCli.java
@@ -8,17 +8,18 @@ public enum ColorCli {
     RESET("\033[0m"),
     CLEAR("\033[H\033[2J"),
 
-    // Regular Colors. Normal color, no bold, background color etc.
+    // Regular Colors. Normal color, no bold, background color etc., for the islands etc...
     RED("\033[0;31m"),      // RED
     GREEN("\033[38;5;28m"),    // GREEN  "\u001B[32m"
     BLUE("\033[0;34m"),     // BLUE
 
-    // Bold
+    // Bold for pawns and towers
     YELLOW_BOLD("\033[1;33m"), // YELLOW
     RED_BOLD("\033[1;31m"),
     GREEN_BOLD("\033[1;32m"),
     PINK_BOLD("\033[35;1m"),
     BLUE_BOLD("\033[1;34m"),
+    
     BLACK_BOLD("\033[1;30m"),
     WHITE_BOLD("\033[37;1m"),
     GRAY_BOLD("\033[30;1m");  //bright black
