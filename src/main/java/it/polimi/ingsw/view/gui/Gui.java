f6769fe139085b80ae32a47b67b40ef7e9239238
Nicolò Scaccia <nicolo.scaccia@mail.polimi.it>
2022-06-17 22:36:22 +0200

    Revert "gui cleanup"
    This reverts commit de03f483

--- src/main/java/it/polimi/ingsw/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Gui.java
@@ -19,6 +19,14 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import it.polimi.ingsw.view.gui.scene.*;
 
+
+/* For java Swing? implements listener ? and extends...
+import javax.swing.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * This class implements all methods of View. Is used for the Graphic User Interface.
  */
@@ -111,6 +119,200 @@ public class Gui extends View {
     public void onAskCharacterParameters(int characterId) {
 
     }
+
+    /* DO DELETE
+   @Override
+    public void askNickname() {
+        Platform.runLater(() -> SceneController.changeRootPane(observers, "login_scene.fxml"));
+    }
+
+    @Override
+    public void askPlayersNumber() {
+        PlayersNumberSceneController pnsc = new PlayersNumberSceneController();
+        pnsc.addAllObservers(observers);
+        pnsc.setPlayersRange(2, 3);
+        Platform.runLater(() -> SceneController.changeRootPane(pnsc, "players_number_scene.fxml"));
+    }
+
+    @Override
+    public void askInitWorkerColor(List<Color> colors) {
+        ColorSceneController csc = new ColorSceneController();
+        csc.addAllObservers(observers);
+        csc.setAvailableColors(colors);
+        Platform.runLater(() -> SceneController.changeRootPane(csc, "color_scene.fxml"));
+    }
+
+    @Override
+    public void askGod(List<ReducedGod> gods, int request) {
+        GodsSceneController gsc = new GodsSceneController();
+        gsc.addAllObservers(observers);
+        gsc.setGods(gods);
+        gsc.setNumberRequest(request);
+        Platform.runLater(() -> SceneController.changeRootPane(gsc, "gods_scene.fxml"));
+    }
+
+    @Override
+    public void askInitWorkersPositions(List<Position> positions) {
+        BoardSceneController bsc = getBoardSceneController();
+        bsc.setAvailablePositionClicks(2);
+        bsc.setSpaceClickType(MessageType.INIT_WORKERSPOSITIONS);
+        Platform.runLater(() -> bsc.setEnabledSpaces(positions));
+    }
+
+    @Override
+    public void askMovingWorker(List<Position> positions) {
+        BoardSceneController bsc = getBoardSceneController();
+        bsc.setAvailablePositionClicks(1);
+        bsc.setSpaceClickType(MessageType.PICK_MOVING_WORKER);
+        Platform.runLater(() -> bsc.setEnabledSpaces(positions));
+    }
+
+    @Override
+    public void askMove(List<Position> positions) {
+        BoardSceneController bsc = getBoardSceneController();
+        bsc.setAvailablePositionClicks(1);
+        bsc.setSpaceClickType(MessageType.MOVE);
+        Platform.runLater(() -> bsc.setEnabledSpaces(positions));
+    }
+
+    @Override
+    public void askBuild(List<Position> positions) {
+        BoardSceneController bsc = getBoardSceneController();
+        bsc.setAvailablePositionClicks(1);
+        bsc.setSpaceClickType(MessageType.BUILD);
+        Platform.runLater(() -> bsc.setEnabledSpaces(positions));
+    }
+
+    @Override
+    public void askMoveFx(List<Position> positions) {
+        BoardSceneController bsc = getBoardSceneController();
+        bsc.setAvailablePositionClicks(1);
+        bsc.setSpaceClickType(MessageType.MOVE_FX);
+        Platform.runLater(() -> bsc.setEnabledSpaces(positions));
+    }
+
+    @Override
+    public void askBuildFx(List<Position> positions) {
+        BoardSceneController bsc = getBoardSceneController();
+        bsc.setAvailablePositionClicks(1);
+        bsc.setSpaceClickType(MessageType.BUILD_FX);
+        Platform.runLater(() -> bsc.setEnabledSpaces(positions));
+    }
+
+    @Override
+    public void showLoginResult(boolean nicknameAccepted, boolean connectionSuccessful, String nickname) {
+        if (!nicknameAccepted || !connectionSuccessful) {
+            if (!nicknameAccepted && connectionSuccessful) {
+                Platform.runLater(() -> {
+                    SceneController.showAlert(STR_ERROR, "Nickname already taken.");
+                    SceneController.changeRootPane(observers, "login_scene.fxml");
+                });
+            } else {
+                Platform.runLater(() -> {
+                    SceneController.showAlert(STR_ERROR, "Could not contact server.");
+                    SceneController.changeRootPane(observers, MENU_SCENE_FXML);
+                });
+            }
+        }
+    }
+
+    @Override
+    public void showGenericMessage(String genericMessage) {
+        Platform.runLater(() -> SceneController.showAlert("Info Message", genericMessage));
+    }
+
+    @Override
+    public void showDisconnectionMessage(String nicknameDisconnected, String text) {
+        Platform.runLater(() -> {
+            SceneController.showAlert("GAME OVER", "The player " + nicknameDisconnected + " disconnected.");
+            SceneController.changeRootPane(observers, MENU_SCENE_FXML);
+        });
+    }
+
+    @Override
+    public void showErrorAndExit(String errorMsg) {
+        Platform.runLater(() -> {
+            SceneController.showAlert(STR_ERROR, errorMsg);
+            SceneController.changeRootPane(observers, MENU_SCENE_FXML);
+        });
+    }
+
+
+    @Override
+    public void showLobby(List<String> nicknameList, int maxPlayers) {
+        LobbySceneController lsc;
+
+        try {
+            lsc = (LobbySceneController) SceneController.getActiveController();
+            lsc.setNicknames(nicknameList);
+            lsc.setMaxPlayers(maxPlayers);
+            Platform.runLater(lsc::updateValues);
+        } catch (ClassCastException e) {
+            lsc = new LobbySceneController();
+            lsc.addAllObservers(observers);
+            lsc.setNicknames(nicknameList);
+            lsc.setMaxPlayers(maxPlayers);
+            LobbySceneController finalLsc = lsc;
+            Platform.runLater(() -> SceneController.changeRootPane(finalLsc, "lobby_scene.fxml"));
+        }
+    }
+
+    @Override
+    public void askEnableEffect(boolean forceApply) {
+        if (forceApply) {
+            // send to server the request to apply my effect.
+            Platform.runLater(() -> notifyObserver(obs -> obs.onUpdateEnableEffect(true)));
+        } else {
+            BoardSceneController bsc = getBoardSceneController();
+            Platform.runLater(() -> bsc.enableEffectControls(true));
+        }
+    }
+
+    @Override
+    public void showMatchInfo(List<String> players, List<ReducedGod> gods, List<Color> colors, String activePlayer) {
+        BoardSceneController bsc = getBoardSceneController();
+        Platform.runLater(() -> bsc.updateMatchInfo(players, gods, colors, activePlayer));
+    }
+
+    @Override
+    public void askFirstPlayer(List<String> nicknameList, List<ReducedGod> gods) {
+        FirstPlayerSceneController fpsc = new FirstPlayerSceneController();
+        fpsc.addAllObservers(observers);
+        fpsc.setNicknames(nicknameList);
+        fpsc.setGods(gods);
+        Platform.runLater(() -> SceneController.changeRootPane(fpsc, "first_player_scene.fxml"));
+    }
+
+    @Override
+    public void showWinMessage(String winner) {
+        Platform.runLater(() -> {
+            SceneController.showWin(winner);
+            SceneController.changeRootPane(observers, MENU_SCENE_FXML);
+        });
+    }
+     @Override
+    public void onUpdateServerInfo(Map<String, String> serverInfo) {
+
+    }
+
+    @Override
+    public void onUpdateNickname(String nickname) {
+
+    }
+
+    @Override
+    public void onUpdatePlayersNumber(int playersNumber) {
+
+    }
+
+    @Override
+    public void onDisconnection() {
+
+    }
+
+
+    }
+*/
 
     /**
      * @param toShow
